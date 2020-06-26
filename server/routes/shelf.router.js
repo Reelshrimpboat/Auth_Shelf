e29f3cac5aa313086d0bf45a2cf118cb0199ebb9
Pablo Valenzuela <p.valenzuela17@gmail.com>
2020-06-26 10:40:56 -0500

    delete saga fetchitem and reducers added

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user);
+    if (req.isAuthenticated()) {
+        pool.query('SELECT * FROM item WHERE item.user_id = $1;', [req.user.id])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for items:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
-
-
 /**
  * Add an item for the logged in user to the shelf
  */
